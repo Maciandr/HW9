d35a177bbc96f80c59bc44ad91b411d397b58d2e
Maciandr <Crazymacia@gmail.com>
2023-04-16 21:36:14 +0300

    Initial commit

---
+++ src/ZooPark.java
@@ -0,0 +1,62 @@
+public class ZooPark extends Animal {
+
+    Animal[] zooParks = new Animal[10];
+
+    public void addAnimalInZoo(Animal abc) {
+        for (int i = 0; i < zooParks.length; i++) {
+            if (zooParks[i] == null) {
+                zooParks[i] = abc;
+                zooParks[i].nomer = i + 1;
+                break;
+            }
+        }
+
+    }
+
+    public int getInfoZoo(Animal[] zooParks) {
+        int num = 0;
+        for (int i = 0; i < zooParks.length; i++) {
+            if (zooParks[i] != null) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    public void delete(int del) {
+        for (int i = 0; i < zooParks.length; i++) {
+            if (zooParks[i].nomer == del) {
+                zooParks[i] = null;
+                break;
+            }
+
+        }
+
+    }
+
+    public String information() {
+        String word = "";
+        for (Animal s : zooParks) {
+            if (s != null) {
+                word = word + s.toSpring() + "\n";
+            }
+        }
+        return word;
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("В зоопарке в клетках слышны голоса животных: ");
+    }
+
+    public void allAnimalsSound() {
+        for (Animal animal : zooParks) {
+            if (animal == null) {
+                continue;
+            } else {
+                animal.sound();
+            }
+        }
+    }
+}
+
